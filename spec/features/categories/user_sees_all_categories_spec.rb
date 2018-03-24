eeafab9c002a9b8cd3c2db3d4a39bc1976ce83a9
hmesander <haleymesander@gmail.com>
2018-03-24 13:52:36 -0600

    Adds route for category index and index view

---
+++ spec/features/categories/user_sees_all_categories_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'User sees all categories' do
+  describe 'a user visits /categories' do
+    it 'they see a list of all categories' do
+      category1 = Category.create!(title: 'Category_Name_1')
+      category2 = Category.create!(title: 'Category_Name_2')
+
+      visit '/categories'
+
+      expect(page).to have_content(category1.title)
+      expect(page).to have_content(category2.title)
+    end
+  end
+end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,23 +1,33 @@
 require 'rails_helper'
 
 describe Category, type: :model do
-  context 'invalid attributes' do
-    it 'is invalid without a title' do
-      category = Category.new()
-      expect(category).to be_invalid
+  describe 'validations' do
+    context 'invalid attributes' do
+      it 'is invalid without a title' do
+        category = Category.new()
+        expect(category).to be_invalid
+      end
+
+      it 'has a unique title' do
+        Category.create!(title: 'Category_Name_1')
+        category = Category.new(title: 'Category_Name_1')
+        expect(category).to be_invalid
+      end
     end
 
-    it 'has a unique title' do
-      Category.create!(title: 'Finance')
-      category = Category.new(title: 'Finance')
-      expect(category).to be_invalid
+    context 'valid attributes' do
+      it 'is valid with a title' do
+        category = Category.new(title: 'Category_Name_1')
+        expect(category).to be_valid
+      end
     end
   end
 
-  context 'valid attributes' do
-    it 'is valid with a title' do
-      category = Category.new(title: 'Finance')
-      expect(category).to be_valid
+  describe 'relationships' do
+    it 'has many jobs' do
+      category = Category.create!(title: 'Category_Name_1')
+      expect(category).to respond_to(:jobs)
     end
   end
+
 end
