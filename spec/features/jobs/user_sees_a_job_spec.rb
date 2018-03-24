76ec5a40860f675d609d41acc0a3aaa945f77c73
alexcutschall <alexander.cutschall@gmail.com>
2018-03-24 12:59:27 -0600

    adding job comments

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -4,11 +4,15 @@ describe "User sees a specific job" do
   scenario "a user sees a job for a specific company" do
     company = Company.create!(name: "ESPN")
     job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    comment_1 = job.job_comments.create!(body: "This is a note!")
+    comment_2 = job.job_comments.create!(body: "This is another note!")
 
     visit company_job_path(company, job)
 
     expect(page).to have_content("ESPN")
     expect(page).to have_content("Developer")
     expect(page).to have_content("70")
+    expect(page).to have_content(comment_1.body)
+    expect(page).to have_content(comment_2.body)
   end
 end
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -33,5 +33,9 @@ describe Job do
       job = Job.new(title: "Software", level_of_interest: 70, description: "Wahooo")
       expect(job).to respond_to(:company)
     end
+    it "has many comments" do
+      job = Job.new(title: "Animation Developer", level_of_interest: 5, description: "Wahooo")
+      expect(job).to respond_to(:job_comments)
+    end
   end
 end
