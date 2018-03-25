91b289113fd91df22deaa1beafe7eeaf74955a03
alexcutschall <alexander.cutschall@gmail.com>
2018-03-25 09:23:19 -0600

    finishing tests

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,7 @@
 class Job < ApplicationRecord
   validates :title, :level_of_interest, :city, presence: true
 
+  belongs_to :category
   belongs_to :company
   has_many :job_comments
 end
--- spec/features/jobs/user_sees_all_jobs_spec.rb
+++ spec/features/jobs/user_sees_all_jobs_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 describe "User sees all jobs" do
   scenario "a user sees all the jobs for a specific company" do
+    category = Category.create!(title: "Management")
     company = Company.create!(name: "ESPN")
-    company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
-    company.jobs.create!(title: "QA Analyst", level_of_interest: 70, city: "New York City")
+    company.jobs.create!(category_id: category.id, title: "Developer", level_of_interest: 70, city: "Denver")
+    company.jobs.create!(category_id: category.id, title: "QA Analyst", level_of_interest: 70, city: "New York City")
 
     visit company_path(company)
 
