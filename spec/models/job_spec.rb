d9a624ef005d694750a079af799f3c6b9833c99a
alexcutschall <alexander.cutschall@gmail.com>
2018-03-25 15:37:18 -0600

    Added sort by location and sort by interest

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -40,15 +40,18 @@ describe Job do
     end
   end
 
-  describe "has comments" do
-    it "can order comments" do
-      category = Category.create!(title: "Management")
-      company = Company.create!(name: "Disney")
-      job = Job.create!(title: "Animation Developer", level_of_interest: 5, description: "Wahooo", city: "Denver", company_id: company.id, category_id: category.id)
-      comment_1 = job.job_comments.create!(body: "This is a comment")
-      comment_2 = job.job_comments.create!(body: "This is another comment")
-
-      expect(job.comments_ordered).to eq(comment_2.body + " " + comment_1.body)
-    end
-  end
+  # describe "has comments" do
+  #   it "can order comments" do
+  #     category = Category.create!(title: "Management")
+  #     company = Company.create!(name: "Disney")
+  #     job = Job.create!(title: "Animation Developer", level_of_interest: 5, description: "Wahooo", city: "Denver", company_id: company.id, category_id: category.id)
+  #     comment_1 = job.job_comments.create!(body: "This is a comment")
+  #     comment_2 = job.job_comments.create!(body: "This is another comment")
+  #
+  #     visit job_path(job)
+  #
+  #     expect(page).to have_content(comment_1)
+  #     expect(page).to have_content(comment_2)
+  #   end
+  # end
 end
