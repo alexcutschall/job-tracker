c1226e165d4d88a78e5325e4abda233a64407e45
alexcutschall <alexander.cutschall@gmail.com>
2018-03-25 13:26:28 -0600

    added a method to order comments and is now displayed newest first

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,4 +4,8 @@ class Job < ApplicationRecord
   belongs_to :category
   belongs_to :company
   has_many :job_comments, dependent: :destroy
+
+  def comments_ordered
+    job_comments.order(created_at: :desc)
+  end
 end
