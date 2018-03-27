1f831d310e69ee98a43a8d1dbf6933406508ad78
hmesander <haleymesander@gmail.com>
2018-03-26 20:30:55 -0600

    Adds tests for top companies by interest level

---
+++ spec/features/dashboard/user_sees_top_companies_by_interest_level_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'User sees top three companies by interest level' do
+  describe 'user visits dashboard page' do
+    it 'shows top three company names by interest level' do
+      category = Category.create!(title: "Tech")
+      company1 = Company.create!(name: "Comcast")
+      company2 = Company.create!(name: "Health Grades")
+      company3 = Company.create!(name: "Red Bull")
+      company4 = Company.create!(name: "Oracle")
+      Job.create!(title: "Software", level_of_interest: 90, description: "Wahooo", city: "Denver", category_id: category.id, company_id: company1.id)
+      Job.create!(title: "Tech Support", level_of_interest: 20, description: "Blub", city: "Denver", category_id: category.id, company_id: company2.id)
+      Job.create!(title: "Intern", level_of_interest: 50, description: "So-So", city: "Denver", category_id: category.id, company_id: company2.id)
+      Job.create!(title: "Director", level_of_interest: 80, description: "Awesome Maybe", city: "Denver", category_id: category.id, company_id: company3.id)
+      Job.create!(title: "Manager", level_of_interest: 10, description: "No Way Jose", city: "Denver", category_id: category.id, company_id: company4.id)
+
+      visit '/dashboard'
+
+      expect(page).to have_content(company1.name)
+      expect(page).to have_content(company2.name)
+      expect(page).to have_content(company3.namet)
+      expect(page).to_not have_content(company4.name)
+    end
+  end
+end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -33,4 +33,25 @@ describe Company do
       expect(company).to respond_to(:contacts)
     end
   end
+
+  describe 'class methods' do
+    describe '.avg_interest_level' do
+      it "finds the top three companies' average job interest level" do
+        category = Category.create!(title: "Tech")
+        company1 = Company.create!(name: "Comcast")
+        company2 = Company.create!(name: "Health Grades")
+        company3 = Company.create!(name: "Red Bull")
+        company4 = Company.create!(name: "Oracle")
+        Job.create!(title: "Software", level_of_interest: 90, description: "Wahooo", city: "Denver", category_id: category.id, company_id: company1.id)
+        Job.create!(title: "Tech Support", level_of_interest: 20, description: "Blub", city: "Denver", category_id: category.id, company_id: company2.id)
+        Job.create!(title: "Intern", level_of_interest: 60, description: "So-So", city: "Denver", category_id: category.id, company_id: company2.id)
+        Job.create!(title: "Director", level_of_interest: 80, description: "Awesome Maybe", city: "Denver", category_id: category.id, company_id: company3.id)
+        Job.create!(title: "Manager", level_of_interest: 10, description: "No Way Jose", city: "Denver", category_id: category.id, company_id: company4.id)
+
+        expect(Job.avg_interest_level[company1.name]).to eq(90)
+        expect(Job.avg_interest_level[company2.name]).to eq(40)
+        expect(Job.avg_interest_level[company3.name]).to eq(80)
+      end
+    end
+  end
 end
